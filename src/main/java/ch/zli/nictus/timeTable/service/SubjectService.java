17a83c579b923499b50e0098a16af3f31c5ef89e
nico <nico.tuscano@adnovum.ch>
2019-10-01 22:56:51 +0200

    Add empty methods to SubjectService

--- src/main/java/ch/zli/nictus/timeTable/service/SubjectService.java
+++ src/main/java/ch/zli/nictus/timeTable/service/SubjectService.java
@@ -1,8 +1,33 @@
 package ch.zli.nictus.timeTable.service;
 
+import ch.zli.nictus.timeTable.model.Subject;
 import ch.zli.nictus.timeTable.repository.SubjectRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SubjectService {
+    private SubjectRepository subjectRepository;
+
+    public SubjectService(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
+
+    public List<Subject> findAll() {
+        return subjectRepository.findAll();
+    }
+
+    public Subject createSubject(Subject subject) {
+        return subjectRepository.saveAndFlush(subject);
+    }
+
+    public void deleteSubjectById(long id) {
+        //TODO: Add delete method
+    }
+
+    public Subject updateSubject(Subject subject, long id) {
+        //TODO: Add update method
+        return null;
+    }
 }
