5d213240bad07498a64553194a0872dc8ab43899
nico <nico.tuscano@adnovum.ch>
2019-10-02 14:30:45 +0200

    Add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO subject VALUES
+('Math', '2019-11-01', '2020-03-01', 'Monday', ''),
+('English', '2019-11-01', '2020-05-01', 'Tuesday', '')
+;
+
+INSERT INTO user VALUE
+('', '', 'root', 'password')
+;
+
+INSERT INTO school_class VALUE
+('', 'Class 1', '')
+;
+
+INSERT INTO  timetable VALUE
+('', '2021-01-01')
+;
